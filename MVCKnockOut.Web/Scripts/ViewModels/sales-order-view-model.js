10db04eff9714a49e86fe4980903e535a04e06ce
gusdewa <gusdewa@gmail.com>
2014-08-19 23:37:40 +0700

    Little commention

--- MVCKnockOut.Web/Scripts/ViewModels/sales-order-view-model.js
+++ MVCKnockOut.Web/Scripts/ViewModels/sales-order-view-model.js
@@ -1,4 +1,11 @@
-﻿SalesOrderViewModel = function(data) {
+﻿/*
+    Create a client-side view model that can be binded to HTML Views
+*/
+
+SalesOrderViewModel = function (data) {
     var self = this;
+
+    // KO Mapping is a plugin for KnockOut JS to
+    // automatically map all properties
     ko.mapping.fromJS(data, {}, self);
 }
