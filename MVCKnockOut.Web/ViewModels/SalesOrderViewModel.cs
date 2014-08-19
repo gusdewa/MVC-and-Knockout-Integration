6338366d09a17bd11018818e21873b9843ca7798
gusdewa <gusdewa@gmail.com>
2014-08-19 23:35:07 +0700

    Create KnockOut Client-Side ViewModel

---
+++ MVCKnockOut.Web/ViewModels/SalesOrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCKnockOut.Web.ViewModels
+{
+    public class SalesOrderViewModel
+    {
+    }
+}
