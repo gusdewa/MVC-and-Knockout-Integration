1a1a01605f1ffb6b19b29d30bcec0ef35191b7f2
gusdewa <gusdewa@gmail.com>
2014-08-20 15:11:16 +0700

    Upgrade to ASP.Net MVC 5
    To handle scaffolding on EF 6

---
+++ MVCKnockOut.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCKnockOut.Web.Startup))]
+namespace MVCKnockOut.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
